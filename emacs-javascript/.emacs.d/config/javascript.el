ef80ad4b176dd9139d9f9a1c34375c702f1f43ec
Tomasz Szarstuk <tom@starstuck.uk>
2018-06-08 14:54:09 +0100

    Use js2-mode for .mjs files

--- emacs-javascript/.emacs.d/config/javascript.el
+++ emacs-javascript/.emacs.d/config/javascript.el
@@ -1,6 +1,7 @@
 (autoload 'js2-mode "js2-mode" nil t)
 
 (add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
+(add-to-list 'auto-mode-alist '("\\.mjs\\'" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("node" . js2-mode))
 (add-to-list 'interpreter-mode-alist '("phantomjs" . js2-mode))
 
