9d80f00552efaaaf828b60e2ef29a6eae4ff4d3b
Tom (starstuck) <tom@starstuck.uk>
2025-06-06 13:08:34 +0100

    Add nongnu ELPA archive

--- .config/emacs/init.d/package.el
+++ .config/emacs/init.d/package.el
@@ -5,12 +5,6 @@
 ;; variables. On new emacs installation packages can be restored by calling
 ;; ‘package-install-selected-packages’.
 
-(when (featurep 'package)
-
-  ;; For time being use alternate marmelade mirror, because official one is having problems with
-  ;; https connection. It seems that most stuff is accessible through melpa nayway.
-  ;;(add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
-  ;;(add-to-list 'package-archives '("marmalade" . "http://marmalade.ferrier.me.uk/packages/"))
-  ;; Stale melpa seem sto be down at the moemen
-  ;;(add-to-list 'package-archives '("melpa" . "http://stable.melpa.org/packages/"))
+(with-eval-after-load 'package
+  (add-to-list 'package-archives '("nongnu" . "http://elpa.nongnu.org/nongnu/"))
   (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/")))
